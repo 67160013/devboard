0b3bc3ae3504b19bb39664f7bf764850c1b272fd
67160013 <67160013@go.buu.ac.th>
2026-03-17 21:50:46 +0700

    feat: add CommentList and toggle comments

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,9 @@
+import CommentList from "./CommentList";
+import { useState } from "react";
+
 function PostCard({ post, isFavorite, onToggleFavorite }) {
+  const [showComments, setShowComments] = useState(false); // ✅ อยู่ตรงนี้
+
   return (
     <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
       <h3>{post.title}</h3>
@@ -7,6 +12,15 @@ function PostCard({ post, isFavorite, onToggleFavorite }) {
       <button onClick={onToggleFavorite}>
         {isFavorite ? "💔 ยกเลิก" : "❤️ ถูกใจ"}
       </button>
+
+      <button
+        onClick={() => setShowComments(!showComments)}
+        style={{ marginLeft: "10px" }}
+      >
+        💬 {showComments ? "ซ่อนความคิดเห็น" : "ดูความคิดเห็น"}
+      </button>
+
+      {showComments && <CommentList postId={post.id} />}
     </div>
   );
 }
